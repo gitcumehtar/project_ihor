69b9d40b1bef32a7fc2a8087bea36dc8eb56cc25
cumehtar <cumehtar@gmail.com>
2019-11-24 23:11:39 +0200

    Final Project 01

---
+++ src/test/java/com/mainacad/magento/TestUI.java
@@ -0,0 +1,38 @@
+package com.mainacad.magento;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestUI {
+
+    public WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe/");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("http://magento.mainacad.com/");
+        //driver.findElement(By.id("xxx").sendKeys("xxx");
+        //driver.findElement(By.cssSelector("yyy")).click();
+        //WebElement lang = driver.findElement(By.id("zzz"));
+        Select lang = new Select(driver.findElement(By.id("select-language")));
+        lang.selectByVisibleText("Automation");
+    }
+
+    @After
+    public void closeUp() {
+        driver.quit();
+    }
+
+
+}
